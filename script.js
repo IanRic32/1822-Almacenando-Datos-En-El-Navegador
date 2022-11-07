cde0aeb2dcc8f3e3538923177bc4392fdedf5cc3
IanRic32 <al2183041529@azc.uam.mx>
2022-11-06 20:21:55 -0600

    local storage and session storage

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
+const addTask = (evento)=> {
+  const list = document.querySelector('[data-list]');
+  const task = createTask(evento);
+  list.appendChild(task);
+}
+
+const taskList = [];
+
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
@@ -11,12 +19,26 @@ const createTask = (evento) => {
   const value = input.value;
   const date = calendar.value;
   const dateFormat = moment(date).format('DD/MM/YYYY');
-  const list = document.querySelector('[data-list]');
+
   const task = document.createElement('li');
   task.classList.add('card');
   input.value = '';
   //backticks
   const taskContent = document.createElement('div');
+  const taskObj = {
+    value,
+    dateFormat
+  } ;
+  taskList.push(taskObj);
+
+  sessionStorage.setItem("task",JSON.stringify(taskList));//Recibe llave y valor
+
+
+  //LocalStorage....
+  localStorage.setItem("task",JSON.stringify(taskObj));//localstorage solo guarda la informacion mas reciente que recibe
+
+
+
 //ocuparemos la libreria moment la cual nos deja estandarizar las fechas
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
@@ -24,11 +46,13 @@ const createTask = (evento) => {
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
   // task.innerHTML = content;
-
+  const dateElement = document.createElement("span") ;
+  dateElement.innerHTML = dateFormat;
   task.appendChild(taskContent);
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener('click', addTask);
