4c5a5f1e2469dab654f21565da18792447a1d692
IanRic32 <al2183041529@azc.uam.mx>
2022-11-06 20:48:09 -0600

    local storage and session storage

--- components/addTask.js
+++ components/addTask.js
@@ -2,38 +2,35 @@ import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
 export const addTask = (evento)=> {
-    const list = document.querySelector('[data-list]');
-    const task = createTask(evento);
-    list.appendChild(task);
-  }
-const createTask = (evento) => {
     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem("task") ||[] );  
+    const list = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const calendar= document.querySelector("[data-form-date]");
-  
+    
     const value = input.value;
     const date = calendar.value;
     const dateFormat = moment(date).format('DD/MM/YYYY');
-  
-    const task = document.createElement('li');
-    task.classList.add('card');
+
     input.value = '';
-    //backticks
-    const taskContent = document.createElement('div');
+    calendar.valur= "";
     const taskObj = {
-      value,
-      dateFormat
-    } ;
+        value,
+        dateFormat
+      } ;
+    const taskList = JSON.parse(localStorage.getItem("task") ||[] );  
     taskList.push(taskObj);
-  
-    sessionStorage.setItem("task",JSON.stringify(taskList));//Recibe llave y valor
-  
-  
+    sessionStorage.setItem("task",JSON.stringify(taskList));//Recibe llave y valor  
     //LocalStorage....
     localStorage.setItem("task",JSON.stringify(taskObj));//localstorage solo guarda la informacion mas reciente que recibe
-  
-  
+
+    const task = createTask(taskObj);
+    list.appendChild(task);
+  };
+const createTask = ({value,dateFormat}) => {  
+    const task = document.createElement('li');
+    task.classList.add('card');
+    //backticks
+    const taskContent = document.createElement('div');
   
   //ocuparemos la libreria moment la cual nos deja estandarizar las fechas
     const titleTask = document.createElement('span');
